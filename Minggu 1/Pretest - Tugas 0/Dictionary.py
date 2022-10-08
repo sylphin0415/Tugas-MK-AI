c240499a76474f7fa5b8195cb00a37c296db467c
SylphinStar <94887300+sylphin0415@users.noreply.github.com>
2022-10-08 21:14:42 +0800

    Update Dictionary.py

--- Minggu 1/Pretest - Tugas 0/Dictionary.py
+++ Minggu 1/Pretest - Tugas 0/Dictionary.py
@@ -11,5 +11,5 @@ dict={
     'tidur' : 'tunung'
 }
 
-terjemahan1, terjemahan2, terjemahan3, terjemahan4, terjemahan5=input("Masukkan kata yang ingin di terjemahkan: ").split('')
+terjemahan1, terjemahan2, terjemahan3, terjemahan4, terjemahan5=input("Masukkan kata yang ingin di terjemahkan: ").split()
 print('terjemahan: ',dict[terjemahan1], dict[terjemahan2], dict[terjemahan3],dict[terjemahan4], dict[terjemahan5])
