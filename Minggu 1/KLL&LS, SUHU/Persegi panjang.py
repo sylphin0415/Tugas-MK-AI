b90b6732e0cb41e2eab69f17b6e7cea859342e59
SylphinStar <94887300+sylphin0415@users.noreply.github.com>
2022-10-08 20:30:59 +0800

    Create Persegi panjang.py

---
+++ Minggu 1/KLL&LS, SUHU/Persegi panjang.py
@@ -0,0 +1,16 @@
+print('''
+=============================================
+Menghitung keliling dan luas persegi panjang
+=============================================
+''')
+panjang=int(input('Masukkan panjang: '))
+lebar=int(input('Masukkan Lebar: '))
+luas=panjang*lebar
+keliling=2*(panjang*lebar)
+print(f'Luas persegi panjang adalah {luas}')
+print(f'Keliling persegi panjang adalah {keliling}')
+print('''
+=============================================
+                Selesai
+=============================================
+''')
